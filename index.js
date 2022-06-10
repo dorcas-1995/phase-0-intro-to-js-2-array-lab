166e4e9d30c5668a626bbe596633032e0e1b1fbe
+dorcus <+wayumulu58@gmail.com>
2022-06-10 22:14:22 +0300

    assignment commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+  cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+  cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+  cats.splice(-1);
+}
+function destructivelyRemoveFirstCat() {
+  cats.splice(0, 1);
+}
+function appendCat(name) {
+  const newcats = cats.slice();
+  newcats.push("Broom");
+  return newcats;
+}
+function prependCat(name) {
+  const newcats = cats.slice();
+  newcats.unshift("Arnold");
+  return newcats;
+}
+function removeLastCat() {
+  const newcats = cats.slice();
+  newcats.splice(-1);
+  return newcats;
+}
+function removeFirstCat() {
+  const newcats = cats.slice();
+  newcats.splice(0, 1);
+  return newcats;
+}
